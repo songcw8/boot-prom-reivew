8974a8026d2cdb6708b214a201dd515320738985
songcw8 <ben990158@gmail.com>
2025-05-26 11:35:18 +0900

    controller

--- src/main/java/org/example/bootpromreivew/controller/PromController.java
+++ src/main/java/org/example/bootpromreivew/controller/PromController.java
@@ -1,4 +1,32 @@
 package org.example.bootpromreivew.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
 public class PromController {
+
+    @GetMapping("/200")
+    public ResponseEntity<Void> get200() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/201")
+    public ResponseEntity<Void> get201() {
+        return ResponseEntity.status(201).build();
+    }
+
+    @GetMapping("/400")
+    public ResponseEntity<Void> get400() {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/wait")
+    public ResponseEntity<Void> getWait() throws InterruptedException {
+        Thread.sleep(new Random().nextInt(1000, 5000));
+        return ResponseEntity.ok().build();
+    }
 }
